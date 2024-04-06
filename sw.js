b13e90d2766e024daae3d59d094defe4d057a0c1
anonym123445678 <166203984+anonym123445678@users.noreply.github.com>
2024-04-06 10:48:15 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Service worker code goes here
+const CACHE_NAME = 'excellence-academy-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-72x72.png',
+  '/icon-96x96.png',
+  '/icon-128x128.png',
+  '/icon-144x144.png',
+  '/icon-152x152.png',
+  '/icon-192x192.png',
+  '/icon-384x384.png',
+  '/icon-512x512.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
